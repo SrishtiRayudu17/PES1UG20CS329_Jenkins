e7a8aa5f808ef665a9d60e173a4fdf53b6d60574
SrishtiRayudu17 <125630848+SrishtiRayudu17@users.noreply.github.com>
2023-02-18 00:55:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Task 4: I am PES1UG20CS329\n";
   return 0;
 }
